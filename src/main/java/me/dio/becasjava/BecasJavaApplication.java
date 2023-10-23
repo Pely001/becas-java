672b2bb738a28a87d330b463f2e1d479db77cdcb
Pely001 <pelypedrohenrique1@gmail.com>
2023-10-23 01:40:20 -0300

    first commit

---
+++ src/main/java/me/dio/becasjava/BecasJavaApplication.java
@@ -0,0 +1,13 @@
+package me.dio.becasjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BecasJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BecasJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/becasjava/BecasJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.becasjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BecasJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
